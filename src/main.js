eef0651a192c8e6d82d27af21fe75efddef04195
Parth Gupta <parth.eng1210@gmail.com>
2024-06-20 18:36:35 +0530

    chore: final commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import ChainInfo from './constantine.config.js';
 import { coin } from "@cosmjs/stargate";
 
 const REGISTRY_CONTRACT = "archway1lr8rstt40s697hqpedv2nvt27f4cuccqwvly9gnvuszxmcevrlns60xw4r";
+const POKE_ARCH_CONTRACT = "archway1mg4wqfx7546d46ddss8tajkhscwc5dhrud494ct99zpusp9qqunssrgezp";
+const NFT_CONTRACT = "archway19qhzpargg63xmdm0p3tt33u8f2t9t48hawddaz6a2kszvm0ekdrqu4udts";
+
+const token_uri = "https://rose-melodic-felidae-510.mypinata.cloud/ipfs/Qmb6b429x7p2toSzmMZuQQ4823qQmxLLJeKzQGhkYsmD5J/"
+const pokemon_list = ["bulbasaur", "mewtwo", "charmander", "pikachu", "squirtle", "hypno"]
 
 window.config = async () => {
     const chainId = ChainInfo.chainId;
@@ -41,6 +46,7 @@ window.registrationCost = async (years = 1) =>{
 }
 
 window.onload = async () => {
+    // console.log(await window.Godot())
     if (!window.getOfflineSignerAuto || !window.keplr) {
         alert("Please install keplr extension");
     } else {
@@ -53,8 +59,6 @@ window.onload = async () => {
                         preferNoSetFee: true,
                     }
                 }
-                await window.getNames()
-                alert(await window.registerName("pokearch"))
             } catch(error) {
                 alert(error)
                 alert("Failed to suggest the chain");
@@ -64,6 +68,13 @@ window.onload = async () => {
         }
     }
 };
+
+window.godotFunctions = {};
+window.externalator = {
+    addGodotFunction: (n,f) => {
+        window.godotFunctions[n] = f;
+    }
+}
 
 window.getNames = async () => {
     const chainId = ChainInfo.chainId;
@@ -84,24 +95,25 @@ window.getNames = async () => {
             REGISTRY_CONTRACT,
             entrypoint
         );
-        alert(query.names[0]);
-        return query.names
+        query.names.map(name => {
+            window.godotFunctions.renderName(name)
+        }) 
     } catch(e) {
-        alert(e)
         return error
     }
 }
 
+// estimate cost and register name if it doesn't exist
 window.registerName = async (name, expiration) => {
     const chainId = ChainInfo.chainId;
-
+    console.log(name)
     await window.keplr.enable(chainId);
 
     const offlineSigner = window.getOfflineSigner(chainId);
     const accounts = await offlineSigner.getAccounts();
     const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
 
-    let cost = await window.registrationCost(expiration); // 2 year registration
+    let cost = await window.registrationCost(expiration);
     // Convert cost to aarch (mainnet) or aconst (testnet)
     console.log(cost)
     let funds = [coin(String(cost), "aconst")];
@@ -109,7 +121,7 @@ window.registerName = async (name, expiration) => {
     try {
         let entrypoint = {
           resolve_record: {
-            name: name+".arch"
+            name: name
           }
         };
         let query = await signingClient.queryContractSmart(
@@ -118,15 +130,16 @@ window.registerName = async (name, expiration) => {
         );
         console.log(query)
         if (query.address === accounts[0].address) {
-            return true
+            window.godotFunctions.continue("true")
+            return
         }
-        return false
+        alert("Name not available")
       } catch(e) {
         console.log(e)
         try {
             let entrypoint = {
                 register: {
-                    name: name
+                    name: name.split(".")[0]
                 }
             };
             
@@ -140,52 +153,264 @@ window.registerName = async (name, expiration) => {
                 funds
             );
             // Tx result
-            return true;
+            window.godotFunctions.continue("true")
+            return
         } catch (err) {
             console.log(err)
-            return false;
+            return;
         }
       }
 }
 
+window.register = async (id) => {
+    console.log(id)
+    var name = id.split("_").join(".")
+    console.log(name)
+    const chainId = ChainInfo.chainId;
 
-// document.sendForm.onsubmit = () => {
-//     (async () => {
-//         const chainId = ChainInfo.chainId;
+    await window.keplr.enable(chainId);
 
-//         await window.keplr.enable(chainId);
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
 
-//         const offlineSigner = window.getOfflineSigner(chainId);
-//         const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
+    let entrypoint = {
+        check_allowance: {
+            addr: accounts[0].address
+        }
+    };
+    
+    // Broadcast tx
+    let allowed = await signingClient.queryContractSmart(
+        POKE_ARCH_CONTRACT,
+        entrypoint,
+    );
+    // if (!allowed) {
+    //     console.log(accounts[0].address)
+    //     entrypoint = {
+    //         add_allowance: {
+    //             addr: accounts[0].address
+    //         }
+    //     }
+    //     let tx = await signingClient.execute(
+    //         accounts[0].address,
+    //         POKE_ARCH_CONTRACT,
+    //         entrypoint,
+    //         "auto",
+    //         "Registering", // Memo
+    //     );
+    // }
+    entrypoint = {
+        get_player: {
+            id: name
+        }
+    };
+    try {
+        let q = await signingClient.queryContractSmart(
+            POKE_ARCH_CONTRACT,
+            entrypoint,
+        );
+        console.log(q)
+        window.godotFunctions.register("false")
+        await Promise.all(q.player.pokemons.map(async pokemon => {
+            entrypoint = {
+                nft_info: {
+                    token_id: pokemon["token_id"].toString()
+                }
+            };
+            let nftData = await signingClient.queryContractSmart(
+                NFT_CONTRACT,
+                entrypoint,
+            );
+            console.log(nftData)
+            const data = await fetch(nftData.token_uri.replace(/^htps:/, 'https:'))
+            const jsonData = await data.json()
+            console.log(jsonData)
+            window.godotFunctions.addPokemon(jsonData["name"], pokemon["health"], pokemon["index"])
+        }))
+    } catch(e) {
+        console.log("error")
+        console.log(e)
+        try {
+            let entrypoint = {
+                register: {
+                id: name
+                }
+            };
+    
+            const msgExecuteContract = {
+                typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+                value: {
+                    sender: accounts[0].address,
+                    contract: POKE_ARCH_CONTRACT,
+                    msg: new TextEncoder().encode(JSON.stringify(entrypoint)),
+                    funds: [], // Optionally specify funds to send with the message
+                },
+            };
+            const fee = await signingClient.calculateFee(accounts[0].address, [msgExecuteContract], '', 1.5);
+            
+            const broadcastResult = await signingClient.signAndBroadcast(accounts[0].address, [msgExecuteContract], fee);
+            console.log(broadcastResult)
+            window.godotFunctions.register("true")
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
-//         const accounts = await offlineSigner.getAccounts();
-//         const destinationAddress = document.sendForm.recipient.value;
+}
 
-//         let amount = new BigNumber(document.sendForm.amount.value);
-//         amount = amount.multipliedBy(new BigNumber('1e18'));
+window.catch = async (id, pokemon, health, curr_pokemon) => {
+    console.log(id, pokemon, health, curr_pokemon)
+    var name = id.split("_").join(".")
+    const chainId = ChainInfo.chainId;
 
-//         const amountFinal = {
-//             denom: 'aconst',
-//             amount: amount.toString(),
-//         }
+    await window.keplr.enable(chainId);
 
-//         const memo = "Transfer token to another account";
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
 
-//         const broadcastResult = await signingClient.sendTokens(
-//             accounts[0].address,
-//             destinationAddress,
-//             [amountFinal],
-//             "auto",
-//             memo,
-//         );
+    let entrypoint = {
+        catch_pokemon: {
+            id: name,
+            token_uri: token_uri + pokemon.toLocaleLowerCase() + ".json",
+            health: parseInt(health),
+            curr_pokemon: parseInt(curr_pokemon)
+        }
+    };
+    console.log(entrypoint)
+    const msgExecuteContract = {
+        typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+        value: {
+            sender: accounts[0].address,
+            contract: POKE_ARCH_CONTRACT,
+            msg: new TextEncoder().encode(JSON.stringify(entrypoint)),
+            funds: [], // Optionally specify funds to send with the message
+        },
+    };
+    // const fee = await signingClient.calculateFee(accounts[0].address, [msgExecuteContract], '', 1.5);
+    // console.log(fee)
+    // Broadcast tx
+    let amount = new BigNumber("0.42");
+    amount = amount.multipliedBy(new BigNumber('1e18'));
+    // let tx = await signingClient.execute(
+    //     accounts[0].address,
+    //     POKE_ARCH_CONTRACT,
+    //     {
+    //         amount: [{
+    //             denom: "aconst",
+    //             amount: amount.toString()
+    //         }],
+    //         gas: amount.toString()
+    //     },
+    //     "Catching Pokemon", // Memo
+    // );
+    const broadcastResult = await signingClient.signAndBroadcast(accounts[0].address, [msgExecuteContract], {
+        amount: [{
+            denom: "uconst",
+            amount: "3000000"
+        }],
+        gas: "3000000"
+    });
 
-//         if (broadcastResult.code !== undefined &&
-//             broadcastResult.code !== 0) {
-//             alert("Failed to send tx: " + broadcastResult.log || broadcastResult.rawLog);
-//         } else {
-//             alert("Succeed to send tx:" + broadcastResult.transactionHash);
-//         }
-//     })();
+}
 
-//     return false;
-// };
+window.setDefault = async (name, pokemon) => {
+    const chainId = ChainInfo.chainId;
+
+    await window.keplr.enable(chainId);
+
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
+
+    let entrypoint = {
+        set_default_pokemon: {
+            id: name.split("_").join("."),
+            pokemon: pokemon_list.indexOf(pokemon.toLocaleLowerCase())
+        }
+    };
+    
+    // Broadcast tx
+    let tx = await signingClient.execute(
+        accounts[0].address,
+        POKE_ARCH_CONTRACT,
+        entrypoint,
+        "auto",
+        "Changing Default Pokemon", // Memo
+    );
+}
+
+window.battle = async (name) => {
+    const chainId = ChainInfo.chainId;
+
+    await window.keplr.enable(chainId);
+
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
+
+    let entrypoint = {
+        get_player: {
+            id: name,
+        }
+    };
+    
+    let query = await signingClient.queryContractSmart(
+        POKE_ARCH_CONTRACT,
+        entrypoint,
+    );
+
+    window.godotFunctions.startBattle(pokemon_list[query.player.default_pokemon].charAt(0).toUpperCase() + pokemon_list[query.player.default_pokemon].slice(1))
+}
+
+window.updateHealth = async (name, pokemon) => {
+    const chainId = ChainInfo.chainId;
+
+    await window.keplr.enable(chainId);
+
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
+
+    let entrypoint = {
+        update_health: {
+            id: name,
+            token_id: pokemon
+        }
+    };
+    
+    // Broadcast tx
+    let tx = await signingClient.execute(
+        accounts[0].address,
+        POKE_ARCH_CONTRACT,
+        entrypoint,
+        "auto",
+        "Updating health", // Memo
+    );
+}
+
+window.collectBerries = async (name) => {
+    const chainId = ChainInfo.chainId;
+
+    await window.keplr.enable(chainId);
+
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const signingClient = await SigningArchwayClient.connectWithSigner(ChainInfo.rpc, offlineSigner);
+
+    let entrypoint = {
+        collect_berries: {
+            id: name
+        }
+    };
+    
+    // Broadcast tx
+    let tx = await signingClient.execute(
+        accounts[0].address,
+        POKE_ARCH_CONTRACT,
+        entrypoint,
+        "auto",
+        "Collecting berries", // Memo
+    );
+}
